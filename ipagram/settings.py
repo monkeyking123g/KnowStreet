0bdb68fde86082b0b28df252c503e89ce2c93b79
dima1233434 <dimaipatii1994@gmail.com>
2022-02-08 20:49:12 +0100

    deploy

--- ipagram/settings.py
+++ ipagram/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from pickle import TRUE
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7jq_5!myx)tz&n9_(uv_#vv1yxlm)xn$ynxzt)u^i503zyd(8p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 
     'rest_framework', # new
     'corsheaders',
+    'django_cleanup',
 ]
 
 AUTH_USER_MODEL = 'base.User'
@@ -144,3 +147,7 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
